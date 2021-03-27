f6cadc467d7af666173569e4bc05f4eba8c67a30
Rich Holzmann <rich.holzmann@gmail.com>
2021-03-27 12:13:51 +0930

    Added outsite.ts module

---
+++ src/accessories/outside.ts
@@ -0,0 +1,49 @@
+import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { MHACWIFI1, MhacModeTypes } from './device';
+import { MitsubishiHeavyAirconPlatform } from '../platform';
+
+export class OutsideTemperature {
+
+    private service: Service;
+    private debounce: any = { speed: null };
+
+    constructor(
+        private readonly platform: MitsubishiHeavyAirconPlatform,
+        accessory: PlatformAccessory,
+        private readonly device: MHACWIFI1
+    ) {
+        let Characteristic = platform.Characteristic;
+
+        // Create the outdoor temperature service
+        // Implemented characteristics:
+        //    CurrentTemperature
+        this.service = accessory.getService(platform.Service.TemperatureSensor) ||
+            accessory.addService(platform.Service.TemperatureSensor, "Outside")
+        this.service.setCharacteristic(Characteristic.Name, "Outside")
+        this.service.getCharacteristic(Characteristic.CurrentTemperature)
+            .onGet(this.getCurrentTemperature.bind(this))
+    }
+
+    updateHomeBridgeState() {
+        this.checkValid()
+        this.syncCharacteristic('CurrentTemperature', this.getCurrentTemperature())
+    }
+
+    syncCharacteristic(characteristic: string, value: number) {
+        if (this.service.getCharacteristic(this.platform.Characteristic[characteristic]).value != value) {
+            this.platform.log.debug(`Updating homebridge characteristics TemperatureSensor.${characteristic} => ${value}`)
+            this.service.getCharacteristic(this.platform.Characteristic[characteristic]).updateValue(value)
+        }
+    }
+
+    private checkValid() {
+        if (!this.device.get.valid())
+            throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE)
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private getCurrentTemperature(): number {
+        this.checkValid()
+        return this.device.get.outdoorTemperature()
+    }
+}
